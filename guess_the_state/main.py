dd170b3d56e4e48fb43d17499f8f3c24044db7fe
Paras Kamble <89475543+Parask31@users.noreply.github.com>
2022-01-22 13:24:56 +0530

    Update main.py

--- guess_the_state/main.py
+++ guess_the_state/main.py
@@ -11,42 +11,28 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 
-data_dict = data.to_dict()
+all_state = data.state.to_list()
 
-state = data_dict["state"]
-x = data_dict["x"]
-y = data_dict["y"]
-
-count = 0
 guessed_state = []
-game_is_on = True
 missed_states = []
 
 
-while count < 50 and game_is_on:
-    answer = screen.textinput(title=f"{count}/50 states guessed", prompt="Whats another state?").title()
+while len(guessed_state) < 50:
+    answer = screen.textinput(title=f"{len(guessed_state)}/50 states guessed", prompt="Whats another state?").title()
 
-    for i in range(0, 50):
-        if answer == "Exit":
-            game_is_on = False
+    if answer == "Exit":
+        missed_state_list = [state for state in all_state if state not in guessed_state]
+        missed_data = pandas.DataFrame(missed_state_list)
+        missed_data.to_csv("states_to_learn.csv")
+        break
 
-            break
-        if answer == state[i]:
-            t = turtle.Turtle()
-            t.hideturtle()
-            t.penup()
-            t.goto(x[i], y[i])
-            t.write(state[i])
-            count += 1
-            guessed_state.append(state[i])
-
-for j in range(0, 50):
-    if state[j] in guessed_state:
-        pass
-    else:
-        missed_states.append(state[j])
-
-    missed_data = pandas.DataFrame(missed_states)
-    missed_data.to_csv("states_to_learn.csv")
+    if answer in all_state:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer)
+        guessed_state.append(answer)
 
 screen.mainloop()
